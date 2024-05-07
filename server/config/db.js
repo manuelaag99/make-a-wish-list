0aa6c4ecd8bcff9144023df7927e670f4b85f1f5
Manuel Alanís <manuelaag99@gmail.com>
2024-05-06 18:23:38 -0600

    functional .env on server

--- server/config/db.js
+++ server/config/db.js
@@ -1,10 +1,9 @@
 const mongoose = require("mongoose");
 
 async function connectDB () {
-    const conn = await mongoose.connect("mongodb+srv://admin-manuelaag99:Nwkg0tEWThBiGdke@cluster0.7cwhb62.mongodb.net/");
+    const conn = await mongoose.connect(process.env.MONGO_URI);
 
     console.log("connected to " + conn.connection.host);
-
 }
 
 module.exports = connectDB;
