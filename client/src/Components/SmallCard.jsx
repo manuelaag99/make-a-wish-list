a85eb516eb5d302e7af28f9bfa0768897d1fbe17
Manuel Alanís <manuelaag99@gmail.com>
2024-05-20 23:09:32 -0600

    functional display of database query

--- client/src/Components/SmallCard.jsx
+++ client/src/Components/SmallCard.jsx
@@ -1,5 +1,24 @@
+import { gql, useQuery } from "@apollo/client";
+
+const GET_USER = gql`
+	query getUsers {
+		user (id: "663eb12306ec274d4c1195ca") {
+			displayName
+			email
+		}
+	}
+`
+
 export default function SmallCard ({}) {
-    return (
+	const { loading, error, data } = useQuery(GET_USER);
+	
+	if (loading) return <p>Loading...</p>
+	
+	if (error) return <p>Error</p>
+
+	if (!loading && !error) console.log(data) 
+    
+    if (data) return (
         <div className='flex flex-col w-full sm:w-35 justify-center items-start bg-white h-fit p-10 shadow-2xl rounded-md'>
             <div className="flex w-full aspect-square">
                 <img className="w-full" src="" alt="" />
@@ -7,7 +26,7 @@ export default function SmallCard ({}) {
             <div className="flex flex-col w-full my-4">
                 <div className="flex w-full">
                     <p className="text-left text-black font-bold overflow-hidden">
-                        Nombre de usuario
+                        {data.user.displayName}
                     </p>
                 </div>
                 <div className="flex w-full">
