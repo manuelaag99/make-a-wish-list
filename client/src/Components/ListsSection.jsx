f1e73d49b83112e9e6aea82c7998fde7176c1c29
Manuel Alanís <manuelaag99@gmail.com>
2024-06-05 11:02:04 -0600

    functional fetching and displaying lists

--- client/src/Components/ListsSection.jsx
+++ client/src/Components/ListsSection.jsx
@@ -1,19 +1,38 @@
+import { gql, useQuery } from "@apollo/client";
 import ListBox from "./ListBox";
 
-export default function ListsSection ({}) {
-    return (
-        <div className="flex flex-col w-full">
-			<div className='flex flex-col w-full rounded-none'>
-				<div className='flex w-full justify-center items-center bg-white hover:bg-gray-300 duration-200 rounded-none py-4 cursor-pointer'>
-					<p className='text-center font-bold overflow-hidden'>
-						+ Agregar lista nueva
-						</p>
-				</div>
-			</div>
+const GET_USER_LISTS = gql`
+	query getUserLists {
+		listsByCreator (creatorId: "6660935f2e128966078f032c") {
+			id
+			listName
+			listDescription
+		}
+	}
+`
 
-			<div className='flex flex-col w-full'>
-				<ListBox listDescription="Descripcion de lista" listName="Nombre de lista" listPrivacy="Privacidad de lista" />
+export default function ListsSection ({}) {
+	const { loading, error, data } = useQuery(GET_USER_LISTS);
+
+	if (error) return <p>Error</p>
+
+	if (!error && !data && loading) return <p>Loading...</p>
+	
+    if (!error && !loading && data && data.listsByCreator.length) {
+		return (
+			<div className="flex flex-col w-full">
+				<div className='flex flex-col w-full rounded-none'>
+					<div className='flex w-full justify-center items-center bg-white hover:bg-gray-300 duration-200 rounded-none py-4 cursor-pointer'>
+						<p className='text-center font-bold overflow-hidden'>
+							+ Agregar lista nueva
+						</p>
+					</div>
+				</div>
+	
+				{data && (data.listsByCreator) && (data.listsByCreator.length > 0) && data.listsByCreator.map((list) => {
+					return <ListBox key={list.id} listName={list.listName} listDescription={list.listDescription} listPrivacy="Privacidad de lista" />
+				})}
 			</div>
-		</div>
-    )
+		)
+	}
 }
