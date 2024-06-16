02b1be48de35ebdfbdbc9cace7e8e7d41171707c
Manuel Alanís <manuelaag99@gmail.com>
2024-06-15 18:36:17 -0600

    add state to 'add content' modal

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -17,13 +17,13 @@ const client = new ApolloClient({
 
 
 export default function App () {
-	const [isAddContentModalVisible, setIsAddContentModalVisible] = useState(false);
+	const [isAddContentModalVisible, setIsAddContentModalVisible] = useState(true);
 
 	return (
 		<ApolloProvider client={client}>
 			<div className='flex sm:h-screen h-full justify-center items-start bg-var-1 inter-font py-32'>
 				<div className='flex w-full fixed bg-var-2 shadow-2xl h-20 top-0 '></div>
-				<div className='flex flex-col sm:flex-row justify-between items-start w-9/10 sm:w-6/10 p-10'>
+				<div className='flex flex-col sm:flex-row justify-between items-start w-9/10 sm:w-6/10 sm:p-10'>
 					<SmallCard />
 					<div className='flex flex-col w-full sm:w-6/10 justify-center mt-5 sm:mt-0'>
 						<LargeCard />
--- client/src/Components/Modals/AddContentModal.jsx
+++ client/src/Components/Modals/AddContentModal.jsx
@@ -1,7 +1,8 @@
+import { useState } from "react";
 import AddButtonSection from "../AddButtonSection";
 
 export default function AddContentModal ({}) {
-    const [typeOfContent, setTypeOfContent] = useState('list')
+    const [typeOfContent, setTypeOfContent] = useState('post')
     return (
         <div>
             <div className="fixed top-0 left-o w-full h-full bg-black opacity-35 z-20"></div>
@@ -9,19 +10,28 @@ export default function AddContentModal ({}) {
                 <div className="flex justify-center items-center w-full sm:mt-8 mt-5 sm:mb-8 mb-2">
                     {(typeOfContent === 'list') && <p className="text-black text-3xl concert-font text-center">Agregar lista</p>}
                     {(typeOfContent === 'element') && <p className="text-black text-3xl concert-font text-center">Agregar elemento</p>}
-                    {(typeOfContent === 'post') && <p className="text-black text-3xl concert-font text-center">Agregar publicacion</p>}
+                    {(typeOfContent === 'post') && <p className="text-black text-3xl concert-font text-center">Agregar publicación</p>}
                 </div>
-                <div className="flex flex-col sm:flex-row w-full my-2 items-center ">
-                    <p className="text-black mr-2 w-fit sm:whitespace-nowrap mb-2 sm:mb-0 ">Nombre de la lista: </p>
-                    <input className="bg-gray-300 px-2 py-1 sm:w-full" type="text" placeholder="Título" />
+
+                <div className={"flex w-full my-2 items-center " + (((typeOfContent === 'list') || (typeOfContent === 'element')) && " sm:flex-row" ) + (!((typeOfContent === 'list') || (typeOfContent === 'element')) && " sm:flex-col" )}>
+                    {(typeOfContent === 'list') && <p className="text-black mr-2 w-fit sm:whitespace-nowrap mb-2 sm:mb-0 ">Nombre de la lista: </p>}
+                    {(typeOfContent === 'element') && <p className="text-black mr-2 w-fit sm:whitespace-nowrap mb-2 sm:mb-0 ">Nombre del elemento: </p>}
+                    {(typeOfContent === 'post') && <p className="text-black mr-2 w-fit sm:whitespace-nowrap mb-2 sm:mb-2 ">Título de publicación: </p>}
+                    <input className="bg-gray-300 px-2 py-1 w-full" type="text" placeholder="Título" />
                 </div>
-                <div className="flex flex-col sm:flex-row w-full my-2 items-center ">
-                    <p className="text-black mr-2 w-fit sm:whitespace-nowrap mb-2 sm:mb-0 ">Descripcion de la lista: </p>
-                    <input className="bg-gray-300 px-2 py-1 sm:w-full" type="text" placeholder="Título" />
+
+                <div className={"flex w-full my-2 items-center " + (((typeOfContent === 'list') || (typeOfContent === 'element')) && " sm:flex-row" ) + (!((typeOfContent === 'list') || (typeOfContent === 'element')) && " sm:flex-col" )}>
+                    {(typeOfContent === 'list') && <p className="text-black mr-2 w-fit sm:whitespace-nowrap mb-2 sm:mb-0 ">Descripcion de la lista: </p>}
+                    {(typeOfContent === 'element') && <p className="text-black mr-2 w-fit sm:whitespace-nowrap mb-2 sm:mb-0 ">Descripcion del elemento: </p>}
+                    {(typeOfContent === 'post') && <p className="text-black mr-2 w-fit sm:whitespace-nowrap mb-2 sm:mb-2 ">Contenido: </p>}
+                    {((typeOfContent === 'list') || (typeOfContent === 'element')) && <input className="bg-gray-300 px-2 py-1 w-full" type="text" placeholder="Título" />}
+                    {(typeOfContent === 'post') && <textarea className="bg-gray-300 px-2 py-1 w-full" type="text" placeholder="Título" />}
                 </div>
-                <div className="flex flex-col sm:flex-row sm:w-full my-2 items-center py-1">
+
+                {((typeOfContent === 'list') || (typeOfContent === 'element')) && <div className="flex flex-col sm:flex-row sm:w-full my-2 items-center py-1">
                     <div className="">
-                        <p className="text-black mr-2 w-fit sm:whitespace-nowrap mb-2 sm:mb-0 ">Privacidad de la lista: </p>
+                        {(typeOfContent === 'list') && <p className="text-black mr-2 w-fit sm:whitespace-nowrap mb-2 sm:mb-0 ">Privacidad de la lista: </p>}
+                        {(typeOfContent === 'element') && <p className="text-black mr-2 w-fit sm:whitespace-nowrap mb-2 sm:mb-0 ">Enlace de foto del elemento: </p>}
                     </div>
                     <div className="flex flex-col">
                         <div>
@@ -33,9 +43,8 @@ export default function AddContentModal ({}) {
                             <label htmlFor="private" >Privado</label>
                         </div>
                     </div>
-                    
-                    
-                </div>
+                </div>}
+
                 <AddButtonSection additionalClassNames=" sm:my-10 my-3" hasDisplayMenu={false} />
             </div>
         </div>
