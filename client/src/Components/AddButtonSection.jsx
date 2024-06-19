a346a968540a14cc873064e83c6694175e0ef163
Manuel Alanís <manuelaag99@gmail.com>
2024-06-18 19:47:34 -0600

    fix position for display menu in 'add button'

--- client/src/Components/AddButtonSection.jsx
+++ client/src/Components/AddButtonSection.jsx
@@ -14,29 +14,31 @@ export default function AddButtonSection ({ additionalClassNames, clickButtonFun
 
     return (
         <div className={"block relative h-22 "}>
-            <button disabled={isButtonActive} className={'flex justify-center w-full disabled:bg-var-2-disabled  bg-var-2 hover:bg-var-2-hovered h-fit py-3 cursor-pointer rounded-md shadow-2xl ' + additionalClassNames} onClick={buttonFunction}>
+            {hasDisplayMenu && isDisplayMenuVisible && <div className="flex flex-col w-full absolute -top-28 bg-white shadow-2xl ">
+                <div onClick={onAddList} className='flex justify-center w-full bg-white hover:bg-gray-300 duration-200 h-18 py-3 cursor-pointer hover:shadow-2xl'>
+                    <p className='text-center text-black concert-font overflow-hidden'>
+                        Agregar lista
+                    </p>
+                </div>
+                <div onClick={onAddListElement} className='flex justify-center w-full bg-white hover:bg-gray-300 duration-200 h-18 py-3 cursor-pointer hover:shadow-2xl'>
+                    <p className='text-center text-black concert-font overflow-hidden'>
+                        Agregar elemento a lista
+                    </p>
+                </div>
+                <div onClick={onAddPost} className='flex justify-center w-full bg-white hover:bg-gray-300 duration-200 h-18 py-3 cursor-pointer hover: shadow-2xl'>
+                    <p className='text-center text-black concert-font overflow-hidden'>
+                        Agregar publicación
+                    </p>
+                </div>
+            </div>}
+
+            <button disabled={isButtonActive} className={'flex justify-center w-full disabled:bg-var-2-disabled  bg-var-2 hover:bg-var-2-hovered h-18 py-3 cursor-pointer rounded-md shadow-2xl ' + additionalClassNames} onClick={buttonFunction}>
                 <p className='text-center text-white concert-font overflow-hidden'>
                     Agregar
                 </p>
             </button>
 
-            {hasDisplayMenu && isDisplayMenuVisible && <div className="flex flex-col absolute top-0 bg-white ">
-                <div onClick={onAddList} className='flex justify-center w-full bg-white hover:bg-gray-300 duration-200 h-fit py-3 cursor-pointer hover:shadow-2xl'>
-                    <p className='text-center text-black concert-font overflow-hidden'>
-                        Agregar lista
-                    </p>
-                </div>
-                <div onClick={onAddListElement} className='flex justify-center w-full bg-white hover:bg-gray-300 duration-200 h-fit py-3 cursor-pointer hover:shadow-2xl'>
-                    <p className='text-center text-black concert-font overflow-hidden'>
-                        Agregar elemento a lista
-                    </p>
-                </div>
-                <div onClick={onAddPost} className='flex justify-center w-full bg-white hover:bg-gray-300 duration-200 h-fit py-3 cursor-pointer hover: shadow-2xl'>
-                    <p className='text-center text-black concert-font overflow-hidden'>
-                        Agregar publicación
-                    </p>
-                </div>
-            </div>}            
+                    
         </div>
     )
 }
