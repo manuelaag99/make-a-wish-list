26d4e29e5fce1a5e6c0269051d48d7c692ffa565
Manuel Alanís <manuelaag99@gmail.com>
2024-06-21 17:59:51 -0600

    add display button to list box

--- client/src/Components/ListBox.jsx
+++ client/src/Components/ListBox.jsx
@@ -1,24 +1,48 @@
+import { useState } from "react";
+import { BsThreeDotsVertical } from "react-icons/bs";
+
 export default function ListBox ({ listName, listDescription, listPrivacy, onClickBox }) {
+
+	function onClickThreeDots () {
+		setAreThreeDotsClicked((prev) => (!prev));
+	}
+
+	const [areThreeDotsClicked, setAreThreeDotsClicked] = useState(false);
+
     return (
-        <div className="flex flex-row w-full px-7 cursor-pointer bg-white hover:bg-gray-300" onClick={onClickBox}>
-			<div className="flex flex-col w-9/10 py-4">
-				<div>
-					<p className="text-left text-black font-bold overflow-hidden">
-						{listName}
-					</p>
+		<>
+			<div className="flex flex-row w-full px-7 cursor-pointer bg-white hover:bg-gray-300 duration-200 relative" >
+				<div className="flex flex-col w-9/10 py-4" onClick={onClickBox}>
+					<div>
+						<p className="text-left text-black font-bold overflow-hidden">
+							{listName}
+						</p>
+					</div>
+					<div>
+						<p className="text-left text-black overflow-hidden">
+							{listDescription}
+						</p>
+					</div>
+					<div>
+						<p className="text-left text-gray-500 overflow-hidden">
+							{listPrivacy}
+						</p>
+					</div>
 				</div>
-				<div>
-					<p className="text-left text-black overflow-hidden">
-						{listDescription}
-					</p>
+				<div className="flex w-1/10 justify-center items-center" onClick={onClickThreeDots}>
+					<button className="flex w-full justify-center items-center text-black hover:text-white">
+						<BsThreeDotsVertical />
+					</button>
 				</div>
-				<div>
-					<p className="text-left text-gray-500 overflow-hidden">
-						{listPrivacy}
-					</p>
-				</div>
+				{areThreeDotsClicked && <div className="absolute flex flex-col bg-white w-24 h-fit right-0 top-[60%] shadow-2xl z-20">
+					<div className="flex justify-center items-center w-full p-1 bg-white hover:bg-gray-300 text-center ">
+						Editar
+					</div>
+					<div className="flex justify-center items-center w-full p-1 bg-white hover:bg-gray-300 text-center ">
+						Borrar
+					</div>
+				</div>}
 			</div>
-			<div className="flex w-1/10"></div>
-		</div>
+		</>
     )
 }
--- client/src/Components/Modals/ListWindowModal.jsx
+++ client/src/Components/Modals/ListWindowModal.jsx
@@ -2,9 +2,22 @@ import { useState } from "react";
 import CellForListElement from "../CellForListElement";
 import { gql, useQuery } from "@apollo/client";
 
+// const GET_LIST_BY_ID = gql`
+// 	query getListById($id: ID!) {
+// 		list(id: $id) {
+// 		  creator {
+// 			id
+// 		  }
+// 		  listName
+// 		  listPrivacy
+// 		  listDescription
+// 		}
+// 	}
+// `
+
 const GET_LIST_BY_ID = gql`
-	query getListById($id: ID!) {
-		list(id: $id) {
+	query getListById {
+		list(id: "6660935f2e128966078f032c") {
 		  creator {
 			id
 		  }
@@ -29,16 +42,19 @@ export default function ListWindowModal ({ listId, onClose }) {
             <div className='flex flex-col items-start bg-white h-[80%] shadow-2xl rounded-md px-12 py-8 z-50 md:w-5/10 w-95 fixed top-[10%] left-[2.5%] md:left-[25%]'>
                 <div className='flex flex-col w-full justify-center'>
                     <p className="text-black text-3xl concert-font text-center mb-3">
-                        {data.list.listName}
+                        Nombre de lista
+                        {/* {data.list.listName} */}
                     </p>
                     <p className="text-gray-300 text-center">
-                        {(data.list.listPrivacy === "public") ? "Lista Pública" : "Lista Privada"}
+                        Privacidad de lista
+                        {/* {(data.list.listPrivacy === "public") ? "Lista Pública" : "Lista Privada"} */}
                     </p>
                 </div>
 
                 <div className="flex flex-col w-full justify-center items-center py-6 px-6">
                     <p className="text-left">
-                        {data.list.listDescription}
+                        Descripción de lista
+                        {/* {data.list.listDescription} */}
                     </p>
                 </div>
 
