7052915cd45e6d71ef4a8c61ed45a1050e1c16b3
Manuel Alanís <manuelaag99@gmail.com>
2024-06-28 17:05:35 -0600

    functional fetch and display of posts

--- client/src/Components/PostsSection.jsx
+++ client/src/Components/PostsSection.jsx
@@ -1,16 +1,28 @@
 import PostBox from "./PostBox";
+import { GET_USER_POSTS } from "../queries/PostQueries";
+import { useQuery } from "@apollo/client";
 
 export default function PostsSection () {
-    return (
-        <div className="flex flex-col w-full">
-			<div className='flex w-full justify-center items-center bg-white hover:bg-gray-300 duration-200 rounded-none py-4 cursor-pointer'>
-				<p className='text-center font-bold overflow-hidden'>
-					+ Agregar publicación nueva
-				</p>
+	const { loading, error, data } = useQuery(GET_USER_POSTS);
+
+	if (error) return <p>Error</p>
+
+	if (!error && loading) return <p>Loading...</p>
+
+	if (!error && !loading && data && data.postsByCreator.length) {
+		return (
+			<div className="flex flex-col w-full">
+				<div className='flex w-full justify-center items-center bg-white hover:bg-gray-300 duration-200 rounded-none py-4 cursor-pointer'>
+					<p className='text-center font-bold overflow-hidden'>
+						+ Agregar publicación nueva
+					</p>
+				</div>
+	
+				{data && (data.postsByCreator) && (data.postsByCreator.length > 0) && data.postsByCreator.map((post, index) => {
+					return <PostBox key={index} post={post} postBody="Contenido de la publicacion" userDisplayName="Nombre e usuario" />
+				})}
+	
 			</div>
-
-			<PostBox post={{ postTitle: "bbbb", postBody: "aaaa" }} postBody="Contenido de la publicacion" userDisplayName="Nombre e usuario" />
-
-		</div>
-    )
+		)
+	}
 }
