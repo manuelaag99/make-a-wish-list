327e9f5a35dc5e53df80b57761d47bc36ee37247
Manuel Alanís <manuelaag99@gmail.com>
2024-06-30 11:30:58 -0600

    create 'list of elements' component

--- client/src/Components/Modals/AddOrUpdateContentModal.jsx
+++ client/src/Components/Modals/AddOrUpdateContentModal.jsx
@@ -7,9 +7,9 @@ import { ADD_LIST } from "../../mutations/ListMutations";
 import { GET_LISTS } from "../../queries/ListQueries";
 import { ADD_POST } from "../../mutations/PostMutations";
 import { GET_POSTS } from "../../queries/PostQueries";
+import ListOfElements from "../ListOfElements";
 
-import { MdEdit } from "react-icons/md";
-import { MdDelete } from "react-icons/md";
+
 
 export default function AddOrUpdateContentModal ({ contentToUpdate, isAdd, onClose, typeOfContent, userId }) {
     let newCreationDate = new Date().toISOString();
@@ -21,8 +21,8 @@ export default function AddOrUpdateContentModal ({ contentToUpdate, isAdd, onClo
             addList(formState.title, formState.privacy, formState.description, userId);
         } else if (typeOfContent === 'post') {
             addPost(formState.title, formState.body, userId, newCreationDate);
-        } else if (typeOfContent === 'element') {
-            console.log("Element added")
+        } else if (typeOfContent === 'item') {
+            console.log("Item added")
         }
         // setIsPopUpWindowVisible(true);
     }
@@ -37,7 +37,7 @@ export default function AddOrUpdateContentModal ({ contentToUpdate, isAdd, onClo
             setFormState({ title: contentToUpdate ? contentToUpdate.listName : null, description: contentToUpdate ? contentToUpdate.listDescription : null, privacy: contentToUpdate ? contentToUpdate.listPrivacy : null });
         } else if (typeOfContent === 'post') {
             setFormState({ title: contentToUpdate ? contentToUpdate.postTitle : null, body: contentToUpdate ? contentToUpdate.postBody : null });
-        } else if (typeOfContent === 'element') {
+        } else if (typeOfContent === 'item') {
             setFormState({ title: contentToUpdate ? contentToUpdate.itemName : null, description: contentToUpdate ? contentToUpdate.itemDescription : null, photoUrl: contentToUpdate ? contentToUpdate.photoUrl : null });
         }
     }, [contentToUpdate, typeOfContent])
@@ -49,7 +49,7 @@ export default function AddOrUpdateContentModal ({ contentToUpdate, isAdd, onClo
     }
 
     function checkFormValidity () {
-        if ((typeOfContent === 'list') || (typeOfContent === 'element')) {
+        if ((typeOfContent === 'list') || (typeOfContent === 'item')) {
             if (formState.title && formState.description && formState.privacy) {
                 setIsButtonInactive(false);
             } else {
@@ -102,31 +102,31 @@ export default function AddOrUpdateContentModal ({ contentToUpdate, isAdd, onClo
                 <div className="flex justify-center items-center w-full md:mt-8 mt-5 md:mb-8 mb-2">
                     {(isAdd) && (typeOfContent === 'list') && <p className="text-black text-3xl concert-font text-center">Agregar lista</p>}
                     {(!isAdd) && (typeOfContent === 'list') && <p className="text-black text-3xl concert-font text-center">Modificar lista</p>}
-                    {(isAdd) && (typeOfContent === 'element') && <p className="text-black text-3xl concert-font text-center">Agregar elemento</p>}
-                    {(!isAdd) && (typeOfContent === 'element') && <p className="text-black text-3xl concert-font text-center">Modificar elemento</p>}
+                    {(isAdd) && (typeOfContent === 'item') && <p className="text-black text-3xl concert-font text-center">Agregar elemento</p>}
+                    {(!isAdd) && (typeOfContent === 'item') && <p className="text-black text-3xl concert-font text-center">Modificar elemento</p>}
                     {(isAdd) && (typeOfContent === 'post') && <p className="text-black text-3xl concert-font text-center">Agregar publicación</p>}
                     {(!isAdd) && (typeOfContent === 'post') && <p className="text-black text-3xl concert-font text-center">Modificar publicación</p>}
                 </div>
 
                 <div className={"flex flex-col w-full my-2 items-center " + (typeOfContent === "post" ? " md:flex-col" : " md:flex-row" )}>
                     {(typeOfContent === 'list') && <p className="text-black mr-2 w-fit md:whitespace-nowrap mb-2 md:mb-0 ">Nombre de la lista: </p>}
-                    {(typeOfContent === 'element') && <p className="text-black mr-2 w-fit md:whitespace-nowrap mb-2 md:mb-0 ">Nombre del elemento: </p>}
+                    {(typeOfContent === 'item') && <p className="text-black mr-2 w-fit md:whitespace-nowrap mb-2 md:mb-0 ">Nombre del elemento: </p>}
                     {(typeOfContent === 'post') && <p className="text-black mr-2 w-fit md:whitespace-nowrap mb-2 md:mb-2 ">Título de publicación: </p>}
                     <input className="bg-gray-300 px-2 py-1 w-full" type="text" name="title" placeholder="Título" onChange={(e) => inputChangeHandler(e)} value={formState.title} />
                 </div>
 
                 <div className={"flex flex-col w-full my-2 items-center " + (typeOfContent === "post" ? " md:flex-col" : " md:flex-row" )}>
                     {(typeOfContent === 'list') && <p className="text-black mr-2 w-fit md:whitespace-nowrap mb-2 md:mb-0 ">Descripcion de la lista: </p>}
-                    {(typeOfContent === 'element') && <p className="text-black mr-2 w-fit md:whitespace-nowrap mb-2 md:mb-0 ">Descripcion del elemento: </p>}
+                    {(typeOfContent === 'item') && <p className="text-black mr-2 w-fit md:whitespace-nowrap mb-2 md:mb-0 ">Descripcion del elemento: </p>}
                     {(typeOfContent === 'post') && <p className="text-black mr-2 w-fit md:whitespace-nowrap mb-2 md:mb-2 ">Contenido: </p>}
-                    {((typeOfContent === 'list') || (typeOfContent === 'element')) && <input className="bg-gray-300 px-2 py-1 w-full" type="text" name="description" placeholder="Título" onChange={(e) => inputChangeHandler(e)} value={formState.description} />}
+                    {((typeOfContent === 'list') || (typeOfContent === 'item')) && <input className="bg-gray-300 px-2 py-1 w-full" type="text" name="description" placeholder="Título" onChange={(e) => inputChangeHandler(e)} value={formState.description} />}
                     {(typeOfContent === 'post') && <textarea className="bg-gray-300 px-2 py-1 w-full" type="text" name="body" placeholder="Título" onChange={(e) => inputChangeHandler(e)}  value={formState.body} />}
                 </div>
 
-                {((typeOfContent === 'list') || (typeOfContent === 'element')) && <div className="flex flex-col md:flex-row md:w-full my-2 items-center py-1">
+                {((typeOfContent === 'list') || (typeOfContent === 'item')) && <div className="flex flex-col md:flex-row md:w-full my-2 items-center py-1">
                     <div className="">
                         {(typeOfContent === 'list') && <p className="text-black mr-2 w-fit md:whitespace-nowrap mb-2 md:mb-0 ">Privacidad de la lista: </p>}
-                        {(typeOfContent === 'element') && <p className="text-black mr-2 w-fit md:whitespace-nowrap mb-2 md:mb-0 ">Enlace de foto del elemento: </p>}
+                        {(typeOfContent === 'item') && <p className="text-black mr-2 w-fit md:whitespace-nowrap mb-2 md:mb-0 ">Enlace de foto del elemento: </p>}
                     </div>
                     {(typeOfContent === 'list') && <div className="flex flex-col md:flex-row">
                         <div>
@@ -138,74 +138,11 @@ export default function AddOrUpdateContentModal ({ contentToUpdate, isAdd, onClo
                             <label htmlFor="private" >Privado</label>
                         </div>
                     </div>}
-                    {(typeOfContent === 'element') && <input className="bg-gray-300 px-2 py-1 w-full" type="text" name="photoUrl" placeholder="Enlace" onChange={(e) => inputChangeHandler(e)}  />}
+                    {(typeOfContent === 'item') && <input className="bg-gray-300 px-2 py-1 w-full" type="text" name="photoUrl" placeholder="Enlace" onChange={(e) => inputChangeHandler(e)}  />}
 
                 </div>}
 
-                {(typeOfContent === 'list') && <div className="flex flex-col w-full justify-start overflow-auto h-28 mt-3">
-                    
-                    <div className="flex flex-row w-full bg-white hover:bg-gray-300 duration-200 cursor-pointer py-1 px-1">
-                        <div className="flex w-2/10 px-2 py-1 ">
-                            <img src="" />
-                        </div>
-                        <div className="flex flex-col w-6/10 text-black text-left ">
-                            <div className="flex w-full font-bold">
-                                Nombre de elemento
-                            </div>
-                            <div className="flex w-full font-normal">
-                                Descripcion de elemento
-                            </div>
-                        </div>
-                        <button className="flex w-1/10 justify-center items-center text-black hover:text-white duration-200">
-                            <MdEdit fontSize={20} />
-                        </button>
-                        <button className="flex w-1/10 justify-center items-center text-black hover:text-white duration-200">
-                            <MdDelete fontSize={20} />
-                        </button>
-                    </div>
-
-                    <div className="flex flex-row w-full bg-white hover:bg-gray-300 duration-200 cursor-pointer py-1 px-1">
-                        <div className="flex w-2/10 ">
-                            
-                        </div>
-                        <div className="flex flex-col w-6/10 text-black text-left ">
-                            <div className="flex w-full font-bold">
-                                Nombre de elemento
-                            </div>
-                            <div className="flex w-full font-normal">
-                                Descripcion de elemento
-                            </div>
-                        </div>
-                        <button className="flex w-1/10 justify-center items-center text-black hover:text-white duration-200">
-                            <MdEdit fontSize={20} />
-                        </button>
-                        <button className="flex w-1/10 justify-center items-center text-black hover:text-white duration-200">
-                            <MdDelete fontSize={20} />
-                        </button>
-                    </div>
-
-                    <div className="flex flex-row w-full bg-white hover:bg-gray-300 duration-200 cursor-pointer py-1 px-1">
-                        <div className="flex w-2/10 ">
-                            
-                        </div>
-                        <div className="flex flex-col w-6/10 text-black text-left ">
-                            <div className="flex w-full font-bold">
-                                Nombre de elemento
-                            </div>
-                            <div className="flex w-full font-normal">
-                                Descripcion de elemento
-                            </div>
-                        </div>
-                        <button className="flex w-1/10 justify-center items-center text-black hover:text-white duration-200">
-                            <MdEdit fontSize={20} />
-                        </button>
-                        <button className="flex w-1/10 justify-center items-center text-black hover:text-white duration-200">
-                            <MdDelete fontSize={20} />
-                        </button>
-                    </div>
-                    
-
-                </div>}
+                {(typeOfContent === 'list') && (!isAdd) && <ListOfElements />}
 
                 <ActionButton additionalClassNames=" md:mt-8 md:mb-10 my-6" isButtondDisabled={isButtonInactive} onClickButtonFunction={addButtonFunction} />
             </div>
