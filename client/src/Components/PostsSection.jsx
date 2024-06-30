8bf98761d5a672115cc18e5c72dcb7122b5db4f9
Manuel Alanís <manuelaag99@gmail.com>
2024-06-30 10:22:30 -0600

    functional 'add post' button

--- client/src/Components/PostsSection.jsx
+++ client/src/Components/PostsSection.jsx
@@ -1,9 +1,12 @@
 import PostBox from "./PostBox";
 import { GET_USER_POSTS } from "../queries/PostQueries";
 import { useQuery } from "@apollo/client";
+import AddOrUpdateContentModal from "./Modals/AddOrUpdateContentModal";
+import { useState } from "react";
 
 export default function PostsSection () {
 	const { loading, error, data } = useQuery(GET_USER_POSTS);
+	const [isAddOrUpdateContentModalVisible, setIsAddOrUpdateContentModalVisible] = useState(false);
 
 	if (error) return <p>Error</p>
 
@@ -13,15 +16,18 @@ export default function PostsSection () {
 		return (
 			<div className="flex flex-col w-full">
 				<div className='flex w-full justify-center items-center bg-white hover:bg-gray-300 duration-200 rounded-none py-4 cursor-pointer'>
-					<p className='text-center font-bold overflow-hidden'>
-						+ Agregar publicación nueva
-					</p>
+					<button onClick={() => setIsAddOrUpdateContentModalVisible(true)}>
+						<p className='text-center font-bold overflow-hidden'>
+							+ Agregar publicación nueva
+						</p>
+					</button>
 				</div>
 	
 				{data && (data.postsByCreator) && (data.postsByCreator.length > 0) && data.postsByCreator.map((post, index) => {
 					return <PostBox key={index} post={post} postBody="Contenido de la publicacion" userDisplayName="Nombre e usuario" />
 				})}
-	
+
+				{isAddOrUpdateContentModalVisible && <AddOrUpdateContentModal isAdd={true} contentToUpdate={null} typeOfContent="post" onClose={() => setIsAddOrUpdateContentModalVisible(false)} />}
 			</div>
 		)
 	}
