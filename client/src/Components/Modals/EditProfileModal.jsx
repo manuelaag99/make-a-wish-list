16e5c1d69d7e9c7f0f7b1ffc1763f6b0bbf0b43b
Manuel Alanís <manuelaag99@gmail.com>
2024-07-18 12:03:32 -0600

    add more fields to 'edit profile' modals

--- client/src/Components/Modals/EditProfileModal.jsx
+++ client/src/Components/Modals/EditProfileModal.jsx
@@ -3,17 +3,35 @@ export default function EditProfileModal ({ onClose}) {
         <>
             <div className="flex fixed top-0 left-0 w-full h-full bg-black opacity-35 z-20" onClick={onClose}></div>
             <div className="flex flex-col md:w-7/10 w-95 h-fit justify-center items-center p-5 fixed md:left-[15%] left-[2.5%] bg-white rounded-md shadow-2xl z-50 ">
-                <div className="flex flex-col w-full justify-center items-center text-center py-5">
+                <div className="flex flex-col w-full justify-center items-center text-center pt-2 pb-5">
                     <p className="text-center concert-font text-3xl">
                         Editar perfil
                     </p>
                 </div>
-                <div className="flex md:flex-row flex-col w-9/10 justify-center items-center">
+                <div className="flex md:flex-row flex-col w-9/10 justify-center items-center my-2">
                     <p className="w-fit md:text-left text-center">
                         Nombre
                     </p>
                     <input className="bg-gray-300 w-full md:ml-3 p-1 outline-none " />
-
+                </div>
+                <div className="flex md:flex-row flex-col w-9/10 justify-center items-center my-2">
+                    <p className="w-fit md:text-left text-center">
+                        Usuario
+                    </p>
+                    <input className="bg-gray-300 w-full md:ml-3 p-1 outline-none " />
+                </div>
+                <div className="flex md:flex-row flex-col w-9/10 justify-center items-center my-2">
+                    <p className="w-fit md:text-left text-center">
+                        Contraseña
+                    </p>
+                    
+                    <div className="flex flex-row bg-gray-300 w-full md:ml-3">
+                        <input className="bg-gray-300 w-full md:ml-3 p-1 outline-none " />
+                        <button className="bg-gray-300 p-1">
+                            
+                        </button>
+                    </div>
+                    
                 </div>
             </div>
         </>
