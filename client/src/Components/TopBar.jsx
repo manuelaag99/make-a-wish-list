5e33ad88e92e908b2222cad6d34c24be37f1353d
Manuel Alanís <manuelaag99@gmail.com>
2024-08-15 22:39:25 -0600

    add logo to top bar

--- client/src/Components/TopBar.jsx
+++ client/src/Components/TopBar.jsx
@@ -1,5 +1,7 @@
 export default function TopBar ({}) {
     return (
-        <div className='flex w-full fixed bg-var-2 shadow-2xl h-20 top-0 z-100 '></div>
+        <div className='flex w-full fixed bg-var-2 shadow-2xl h-24 top-0 z-100 '>
+            <img src="../../public/LOGO-WHITE.png" alt='logo' className='h-16 ml-5 my-auto' />
+        </div>
     )
 }
