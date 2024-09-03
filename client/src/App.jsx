f15183de4ecd04f7a4858816ecf79e107fc9532a
Manuel Alanís <manuelaag99@gmail.com>
2024-09-02 20:47:33 -0600

    add authcontext and navigation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,8 @@ import SignInSignUpPage from './pages/SignInSignUpPage';
 import FeedPage from './pages/FeedPage';
 import { SearchProvider } from './Context/SearchQueryContext';
 import SearchResultsPage from './pages/SearchResultsPage';
+import { AuthContext } from './Context/AuthContext';
+import { useCallback, useEffect, useState } from 'react';
 
 const cache = new InMemoryCache();
 
@@ -15,21 +17,75 @@ const client = new ApolloClient({
 	cache	
 })
 
+let logOutTimer;
 export default function App () {
+	const [token, setToken] = useState(false);
+	const [userId, setUserId] = useState(null);
+	const [tokenExpiration, setTokenExpiration] = useState(null);
 	
-	return (
-		<ApolloProvider client={client} >
-			<SearchProvider>
-				<Router>
-					<Routes>
-						<Route path='/' element={<SignInSignUpPage />} />
-						<Route path='feed' element={<FeedPage />} />
-						<Route path='/profile' element={<ProfilePage />} />
-						<Route path='/error' element={<ErrorPage />} />
-						<Route path='/searchresults' element={<SearchResultsPage />} />
-					</Routes>
-				</Router>
-			</SearchProvider>
-		</ApolloProvider>
-	)
+	const logIn = useCallback((uId, token, expirationDate) => {
+		setUserId(uId);
+		setToken((token));
+		const tokenExpirationDate = expirationDate || new Date(new Date().getTime() + 1000 * 60 * 60);
+		setTokenExpiration(tokenExpirationDate);
+		localStorage.setItem("userData", JSON.stringify({ userId: uId, token: token, expiration: tokenExpirationDate.toISOString() }));
+	}, []);
+
+	const logOut = useCallback(() => {
+		setToken(null);
+		setTokenExpiration(null);
+		setUserId(null);
+		localStorage.removeItem("userData");
+	}, []);
+
+	useEffect(() => {
+		if (token && tokenExpiration) {
+			const remainingTime = tokenExpiration.getTime() - new Date().getTime();
+			logOutTimer = setTimeout(logOut, remainingTime);
+		} else {
+			clearTimeout(logOutTimer);
+		}
+	}, [token, logOut, tokenExpiration]);
+
+	useEffect(() => {
+		const storedData = JSON.parse(localStorage.getItem("userData"));
+		if (storedData && storedData.token && (new Date(storedData.expiration) > new Date())) {
+			logIn(storedData.userId, storedData.token, new Date(storedData.expiration));
+		}
+	}, [logIn]);
+	
+	if (!token) {
+		return (
+			<ApolloProvider client={client} >
+				<AuthContext.Provider value={{ isLoggedIn: token, token: token, userId: userId, login: logIn, logout: logOut }} >
+					<SearchProvider>
+						<Router>
+							<Routes>
+								<Route path='/*' element={<SignInSignUpPage />} />
+								<Route path='/error' element={<ErrorPage />} />
+							</Routes>
+						</Router>
+					</SearchProvider>
+				</AuthContext.Provider>
+			</ApolloProvider>
+		)
+	} else {
+		return (
+			<ApolloProvider client={client} >
+				<AuthContext.Provider value={{ isLoggedIn: token, token: token, userId: userId, login: logIn, logout: logOut }} >
+					<SearchProvider>
+						<Router>
+							<Routes>
+								<Route path='/' element={<FeedPage />} />
+								<Route path='feed' element={<FeedPage />} />
+								<Route path='/profile' element={<ProfilePage />} />
+								<Route path='/error' element={<ErrorPage />} />
+								<Route path='/searchresults' element={<SearchResultsPage />} />
+							</Routes>
+						</Router>
+					</SearchProvider>
+				</AuthContext.Provider>
+			</ApolloProvider>
+		)
+	}
 }
